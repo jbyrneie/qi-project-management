7427ea729637afceb0f4d3fb9329ec7376a0b708
Jack Byrne <jbyrne@glgroup.com>
2018-09-23 17:04:12 +0100

    highlight which route is being used

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,12 +1,15 @@
 import React, { Component} from 'react'
 import { observer, inject } from 'mobx-react'
+import views from '../views'
 import lightBox from'../images/lightBox.png'
+import darkBox from'../images/darkBox.png'
 
 // Custom Styles
 import '../css/qi.css'
 
 // MUI
-import {Card, CardText} from 'material-ui/Card'
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 
 class SideBar extends Component {
   constructor() {
@@ -15,19 +18,26 @@ class SideBar extends Component {
     }
   }
 
+  _navigate(route, event) {
+    console.log('_navigate clicked');
+    const {router: {goTo}} = this.props.store
+    goTo(views[route], {}, this.props.store)
+  }
+
   render() {
     const boxStyle = {
       width: '100%',
-      paddingTop: 40
+      paddingTop: 40,
     }
-    
+
     return(
       <div className='sidebar-content'>
-        <Card>
-          <CardText>
+        <Card style={{boxShadow: 0}}>
+          <CardContent>
             <div style={{paddingTop:10, position: 'relative'}}>
               <div style={{fontSize:'36px', color:'#A0A0A0', fontWeight:400, paddingBottom:40}}>QI</div>
-              <img src={lightBox} alt='box' style={boxStyle}/>
+              <img src={this.props.myTasks?darkBox:lightBox} alt='box' style={boxStyle} onClick={this._navigate.bind(this, 'home')}/>
+              <img src={this.props.surveyLead?darkBox:lightBox} alt='box' style={boxStyle} onClick={this._navigate.bind(this, 'surveyLead')}/>
               <img src={lightBox} alt='box' style={boxStyle}/>
               <img src={lightBox} alt='box' style={boxStyle}/>
               <img src={lightBox} alt='box' style={boxStyle}/>
@@ -35,7 +45,7 @@ class SideBar extends Component {
               <img src={lightBox} alt='box' style={boxStyle}/>
               <img src={lightBox} alt='box' style={boxStyle}/>
             </div>
-          </CardText>
+          </CardContent>
         </Card>
       </div>
     )
