7791226c0d52976c737e64cac0711060d4daf053
jbyrneie <jbyrne@glgroup.com>
2018-10-01 13:23:24 +0100

    routes

--- src/views.js
+++ src/views.js
@@ -1,5 +1,13 @@
 import React from 'react'
 import { Route } from 'mobx-router'
+import SurveyLead from './components/SurveyLead'
+import Invites from './components/Invites'
+import Quote from './components/Quote'
+import Settings from './components/Settings'
+import Feasability from './components/Feasability'
+import Programming from './components/Programming'
+import Reporting from './components/Reporting'
+import Fielding from './components/Fielding'
 import Home from './components/Home'
 
 let prefix=''
@@ -14,6 +22,38 @@ const views = {
   home: new Route({
     path: `${prefix}/`,
     component: <Home/>
+  }),
+  feasability: new Route({
+    path: `${prefix}/feasability`,
+    component: <Feasability/>
+  }),
+  fielding: new Route({
+    path: `${prefix}/fielding`,
+    component: <Fielding/>
+  }),
+  invites: new Route({
+    path: `${prefix}/invites`,
+    component: <Invites/>
+  }),
+  programming: new Route({
+    path: `${prefix}/programming`,
+    component: <Programming/>
+  }),
+  quote: new Route({
+    path: `${prefix}/quote`,
+    component: <Quote/>
+  }),
+  reporting: new Route({
+    path: `${prefix}/reporting`,
+    component: <Reporting/>
+  }),
+  settings: new Route({
+    path: `${prefix}/settings`,
+    component: <Settings/>
+  }),
+  surveyLead: new Route({
+    path: `${prefix}/surveyLead`,
+    component: <SurveyLead/>
   }),
   catchall:new Route({
     path:`${prefix}/:def`,
